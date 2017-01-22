a5f6d389b4f52586de493290131f17b41e6bb52d
Walt <wxwsk8er@gmail.com>
2017-01-21 20:28:14 -0500

    Initial commit to development

---
+++ src/me/waltster/Jem/Player.java
@@ -0,0 +1,47 @@
+package me.waltster.Jem;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Player {
+	public float yaw = 0.0f, pitch = 0.0f;
+	private float walkingSpeed = 01.5f;
+	public Vector3d position;
+	
+	public Player(){
+		this.position = new Vector3d(0.0f, -64.0f, 0.0f);
+	}
+	
+	public void render(){
+		glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+		glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+		glTranslated(position.x, position.y, position.z);
+	}
+	
+	public void yaw(float y){
+		yaw += y;
+	}
+	
+	public void pitch(float p){
+		pitch += p;
+	}
+	
+	public void walkForward(){
+		position.x -= walkingSpeed * (float)Math.sin(Math.toRadians(yaw));
+		position.z += walkingSpeed * (float)Math.cos(Math.toRadians(yaw));
+	}
+	
+	public void walkBackwards(){
+		position.x += walkingSpeed * (float)Math.sin(Math.toRadians(yaw));
+		position.z -= walkingSpeed * (float)Math.cos(Math.toRadians(yaw));
+	}
+	
+	public void strafeLeft(){
+		position.x -= walkingSpeed * (float)Math.sin(Math.toRadians(yaw - 90));
+		position.z -= walkingSpeed * (float)Math.cos(Math.toRadians(yaw - 90));
+	}
+	
+	public void strafeRight(){
+		position.x -= walkingSpeed * (float)Math.sin(Math.toRadians(yaw + 90));
+		position.z += walkingSpeed * (float)Math.cos(Math.toRadians(yaw + 90));
+	}
+}
